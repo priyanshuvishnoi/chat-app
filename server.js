e7f481d64a4480abaff26a05af7e6cc26a0221b7
Priyanshu Vishnoi <priyanshuvishnoi990@gmail.com>
2021-02-11 23:43:34 +0530

    Basic functionality complete

---
+++ server.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const logger = require('morgan');
+const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
+const {
+  userJoin,
+  getCurrentUser,
+  getRoomUsers,
+  userLeave,
+} = require('./utils/users');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+const bot = 'Chat-Bot';
+
+app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', socket => {
+  //   console.log('New WS Connection...');
+  socket.on('joinRoom', ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
+    socket.join(user.room);
+
+    socket.emit('message', formatMessage(bot, 'Welcome to Chat-App'));
+
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        'message',
+        formatMessage(bot, `${user.username} has joined the chat`)
+      );
+
+    io.to(user.room).emit('roomUsers', {
+      room: user.room,
+      users: getRoomUsers(user.room),
+    });
+  });
+
+  socket.on('chatMessage', msg => {
+    const user = getCurrentUser(socket.id);
+    io.to(user.room).emit('message', formatMessage(user.username, msg));
+  });
+
+  socket.on('disconnect', () => {
+    const user = userLeave(socket.id);
+
+    if (user) {
+      io.to(user.room).emit(
+        'message',
+        formatMessage(bot, `${user.username} has left the chat`)
+      );
+    }
+
+    io.to(user.room).emit('roomUsers', {
+      room: user.room,
+      users: getRoomUsers(user.room),
+    });
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
